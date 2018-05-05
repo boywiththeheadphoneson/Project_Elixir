c9ff2a0dcc9243fffc96d847c7d9143dc4bc3539
boywiththeheadphoneson <praveensimha19@gmail.com>
2018-05-06 01:47:08 +0530

    added greet.py

---
+++ Project_Elixir/greet.py
@@ -0,0 +1,9 @@
+#Program to greet Vivek and Ajin
+
+user = input("Who be thou?")
+
+if (user == 'Vivek') | (user== 'Ajin'):
+    print("Welcome, " + str(user))
+	
+else:
+    print ("You're not welcome here")
