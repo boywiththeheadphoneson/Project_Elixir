fde038573aaf707c9266a1a0ae81885d3aa3ef06
Vivek Kartha <vivek.kartha@mobme.in>
2018-05-06 01:54:46 +0530

    Removed unecessary spacing

--- Project_Elixir/greet.py
+++ Project_Elixir/greet.py
@@ -1,9 +1,7 @@
 #Program to greet Vivek and Ajin
 
 user = input("Who be thou?")
-
 if (user == 'Vivek') | (user== 'Ajin'):
     print("Welcome, " + str(user))
-	
 else:
     print ("You're not welcome here")
