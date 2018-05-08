392f345b729101487b59d899b1f63772837766fd
boywiththeheadphoneson <praveensimha19@gmail.com>
2018-05-09 02:20:49 +0530

    added changelog.md

--- Classifier.py
+++ Classifier.py
@@ -1,8 +1,8 @@
 from sklearn import tree
 
 
-features = [[140, 1], [130,1], [150, 0], [170, 0]]
-labels = [0, 0, 1, 1]
+features = [[140, "smooth"], [130,"smooth"], [150, "bumpy"], [170, "bumpy"]]
+labels = ["apple", "apple", "orange", "orange"]
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(features, labels)
-print (clf.predict ([[150, 0]]))
+print (clf.predict ([[150, "bumpy"]]))
